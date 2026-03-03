e6a8707189800c3638e21513f9673a3ea3d545da
manshilodhi30-gif <manshilodhi30@gmail.com>
2026-03-03 16:45:59 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0605-can-place-flowers/0605-can-place-flowers.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        for(int i=0; i<flowerbed.length; i++){
+            if(flowerbed[i]==0){
+                boolean l= (i==0) || (flowerbed[i-1]==0);
+                boolean r= (i<flowerbed.length-1) || (flowerbed[i+1]==0);
+                if(l && r){
+                    flowerbed[i]=1;
+                    n--;
+                    if(n==0)return true;
+                }
+                
+            }
+        }
+        return n<=0;
+    }
+}
