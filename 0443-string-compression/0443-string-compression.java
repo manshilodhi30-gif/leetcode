4c4f39f31e0b6be41cd102e3e53386be377035d0
manshilodhi30-gif <manshilodhi30@gmail.com>
2026-03-05 12:11:45 +0530

    Time: 1 ms (99.72%), Space: 45.6 MB (48.13%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int compress(char[] chars) {
+         int write = 0; // position to write compressed characters
+        int read = 0;  // position to read characters
+
+        while (read < chars.length) {
+            char currentChar = chars[read];
+            int count = 0;
+
+            // Count consecutive occurrences
+            while (read < chars.length && chars[read] == currentChar) {
+                read++;
+                count++;
+            }
+
+            // Write the character
+            chars[write++] = currentChar;
+
+            // Write the count if > 1
+            if (count > 1) {
+                for (char c : String.valueOf(count).toCharArray()) {
+                    chars[write++] = c;
+                }
+            }
+        }
+
+        return write;
+    }
+}
