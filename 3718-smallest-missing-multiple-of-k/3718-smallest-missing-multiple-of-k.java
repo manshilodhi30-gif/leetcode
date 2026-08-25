5c7fab65abcc189596aeb308a9c65b13c8483ea6
manshilodhi30-gif <manshilodhi30@gmail.com>
2026-08-25 21:35:22 +0530

    Time: 2 ms (68.2%), Space: 45.1 MB (66.75%) - LeetHub

---
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.java
@@ -0,0 +1,15 @@
+import java.util.HashSet;
+
+class Solution {
+    public int missingMultiple(int[] nums, int k) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            set.add(num);
+        }
+        int current = k;
+        while (set.contains(current)) {
+            current += k;
+        }
+        return current;
+    }
+}
