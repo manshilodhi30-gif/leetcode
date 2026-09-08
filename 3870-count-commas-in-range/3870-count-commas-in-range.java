dafd9481932c76973ce882ce38d15395e0044110
manshilodhi30-gif <manshilodhi30@gmail.com>
2026-09-08 22:01:39 +0530

    Time: 1 ms (83.65%), Space: 42.4 MB (85.26%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.java
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -1,10 +1,12 @@
 class Solution {
     public int countCommas(int n) {
-       int res=0;
-        if(n>=1000)res+=Math.min(n,999999)-999;
-        if(n>=1000000)res+=(Math.min(n,99999999)-999999)*2;
-        if(n>=100000000)res+=(Math.min(n,999999999)-99999999)*3;
-        return res;
-
+        int count=0;
+        if(n >= 1000){
+            count+=n-1000+1;
+        }
+        if(n >= 1000000){
+            count+= n-1000000+1;
+        }
+        return count;
     }
 }
