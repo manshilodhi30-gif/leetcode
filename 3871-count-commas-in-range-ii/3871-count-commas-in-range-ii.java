0fb2947718b228a8a39b09977a34d124852397f0
manshilodhi30-gif <manshilodhi30@gmail.com>
2026-09-09 22:27:55 +0530

    Time: 1 ms (99.36%), Space: 42.8 MB (37.18%) - LeetHub

--- 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.java
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.java
@@ -1,11 +1,17 @@
 class Solution {
     public long countCommas(long n) {
-long nalverqito=n,ans=0;
-if(nalverqito>=1000)ans+=Math.min(nalverqito,999999)-999;
-if(nalverqito>=1000000)ans+=(Math.min(nalverqito,999999999)-999999)*2;
-if(nalverqito>=1000000000)ans+=(Math.min(nalverqito,999999999999L)-999999999)*3;
-if(nalverqito>=1000000000000L)ans+=(Math.min(nalverqito,999999999999999L)-999999999999L)*4;
-if(nalverqito>=1000000000000000L)ans+=(nalverqito-999999999999999L)*5;
-return ans;  
-}
+        long totalCommas = 0;
+        long threshold = 1_000L;
+
+        while (n >= threshold) {
+            totalCommas += (n - threshold + 1);
+            
+            if (threshold > Long.MAX_VALUE / 1000) {
+                break;
+            }
+            threshold *= 1000L;
+        }
+
+        return totalCommas;
+    }
 }
